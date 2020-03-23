77d66e07f8673928fd4a80b1b3136ff3c133172a
jack <jack>
2020-03-23 13:18:49 +0100

    prod-build

--- packages/angular/lib/control.js
+++ packages/angular/lib/control.js
@@ -23,15 +23,6 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("jsonforms/packages/core");
 var core_2 = require("@angular/core");
@@ -89,7 +80,10 @@ var JsonFormsControl = /** @class */ (function (_super) {
         this.triggerValidation();
     };
     // @ts-ignore
-    JsonFormsControl.prototype.mapAdditionalProps = function (props) {
+    // @ts-ignore
+    JsonFormsControl.prototype.mapAdditionalProps = 
+    // @ts-ignore
+    function (props) {
         // do nothing by default
     };
     JsonFormsControl.prototype.ngOnDestroy = function () {
@@ -123,18 +117,11 @@ var JsonFormsControl = /** @class */ (function (_super) {
         this.form.markAsTouched();
         this.form.updateValueAndValidity();
     };
-    __decorate([
-        core_2.Input(),
-        __metadata("design:type", String)
-    ], JsonFormsControl.prototype, "id", void 0);
-    __decorate([
-        core_2.Input(),
-        __metadata("design:type", Boolean)
-    ], JsonFormsControl.prototype, "disabled", void 0);
-    __decorate([
-        core_2.Input(),
-        __metadata("design:type", Boolean)
-    ], JsonFormsControl.prototype, "visible", void 0);
+    JsonFormsControl.propDecorators = {
+        "id": [{ type: core_2.Input },],
+        "disabled": [{ type: core_2.Input },],
+        "visible": [{ type: core_2.Input },],
+    };
     return JsonFormsControl;
 }(base_renderer_1.JsonFormsBaseRenderer));
 exports.JsonFormsControl = JsonFormsControl;
--- packages/angular/lib/jsonforms.component.js
+++ packages/angular/lib/jsonforms.component.js
@@ -12,43 +12,10 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-/*
-  The MIT License
-  
-  Copyright (c) 2018 EclipseSource Munich
-  https://github.com/eclipsesource/jsonforms
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-  
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-  
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-*/
 var maxBy_1 = __importDefault(require("lodash/maxBy"));
 var core_1 = require("@angular/core");
 var core_2 = require("jsonforms/packages/core");
@@ -126,19 +93,20 @@ var JsonFormsOutlet = /** @class */ (function (_super) {
             this.subscription.unsubscribe();
         }
     };
-    __decorate([
-        core_1.Input(),
-        __metadata("design:type", Object),
-        __metadata("design:paramtypes", [Object])
-    ], JsonFormsOutlet.prototype, "renderProps", null);
-    JsonFormsOutlet = __decorate([
-        core_1.Directive({
-            selector: 'jsonforms-outlet'
-        }),
-        __metadata("design:paramtypes", [core_1.ViewContainerRef,
-            core_1.ComponentFactoryResolver,
-            store_1.NgRedux])
-    ], JsonFormsOutlet);
+    JsonFormsOutlet.decorators = [
+        { type: core_1.Directive, args: [{
+                    selector: 'jsonforms-outlet'
+                },] },
+    ];
+    /** @nocollapse */
+    JsonFormsOutlet.ctorParameters = function () { return [
+        { type: core_1.ViewContainerRef, },
+        { type: core_1.ComponentFactoryResolver, },
+        { type: store_1.NgRedux, },
+    ]; };
+    JsonFormsOutlet.propDecorators = {
+        "renderProps": [{ type: core_1.Input },],
+    };
     return JsonFormsOutlet;
 }(base_renderer_1.JsonFormsBaseRenderer));
 exports.JsonFormsOutlet = JsonFormsOutlet;
