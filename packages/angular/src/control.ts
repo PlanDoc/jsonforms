62fcee31f7928ca2585f04ae48ac5db5ca8494cd
jack <jack>
2020-05-20 11:43:11 +0200

    prod-build

--- packages/angular/src/control.ts
+++ packages/angular/src/control.ts
@@ -93,8 +93,13 @@ export class JsonFormsControl extends JsonFormsBaseRenderer<ControlElement>
         this.enabled = enabled;
         this.hidden = !visible;
 
+        console.log(this.uischema);
         if(this.uischema && this.uischema.selector) {
+          console.log(this.uischema.selector);
           let selectorVal = this.uischema.selector(this.uischema.scope);
+          console.log(this.uischema.scope);
+          console.log(toDataPath(this.uischema.scope));
+          console.log(selectorVal);
           if(selectorVal != null) {
             this.visible = selectorVal != FieldPhaseSelector.HIDDEN;
             this.hidden = selectorVal == FieldPhaseSelector.HIDDEN;
